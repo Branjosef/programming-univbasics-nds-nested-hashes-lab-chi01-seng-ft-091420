5d3f06b0459204e506aa1d6bc189a2a8ad68b0e9
Brandon Fonnemann <branjosef@gmail.com>
2020-08-25 02:31:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,11 +18,12 @@ def literal_aoh
   # held in the constants
   literal_aoh = [
           { name:  "Don Gately", occupation: "Live-in Staff" },
-          { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+          { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+          { name:  "Pat Monteseian", occupation: "Staff" },
+          { name:  "Kate Gompert", occupation: "None" },
           
           
-    
-    
+
     ]
 end
 
